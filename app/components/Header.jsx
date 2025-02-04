fbff8db550d25eac2a28d18aac30b2972bde5ddd
Ambika-Rama-Katyayini <ambikaramakatyayini.k@gmail.com>
2025-02-04 22:27:11 +0530

    portfolio updated

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -45,33 +45,50 @@
 import { assets } from '@/assets/assets';
 import Image from 'next/image';
 import React from 'react';
+import { motion } from "motion/react"
 
-const Header = ({isDarkMode}) => {
+const Header = ({ isDarkMode }) => {
     return (
         // <div className="bg-gradient-to-r from-green-300 via-yellow-400 to-green-300 text-black p-6 md:p-12 rounded-lg shadow-lg">
         <div className="p-6 md:p-12 rounded-lg shadow-lg">
 
-            <div className="flex justify-center mb-6">
+            <motion.div className="flex justify-center mb-6"
+                initial={{ scale: 0 }}
+                whileInView={{ scale: 1 }}
+                transition={{ duration: 0.8, type: 'spring', stiffness: 100 }}
+            >
                 <Image
                     src={assets.profile_img}
                     alt="Profile Picture"
                     className="w-32 mt-11"
                 />
-            </div>
-            <h3 className="flex items-center justify-center gap-2 text-xl md:text-2xl mb-3 font-Ovo dark:bg-transparent">
+            </motion.div>
+            <motion.h3
+                initial={{ y: -20, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.3 }}
+                className="flex items-center justify-center gap-2 text-xl md:text-2xl mb-3 font-Ovo dark:bg-transparent">
                 Hi, I'm <p className='font-Outfit'>Ambika Rama Katyayini</p>
                 <Image
-                    src={isDarkMode?assets.hand_icon_dark:assets.hand_icon}
+                    src={isDarkMode ? assets.hand_icon_dark : assets.hand_icon}
                     alt="Hand Icon"
                     className="w-7 animate-wave"
                 />
-            </h3>
-            <h1 className="text-center text-3xl sm:text-5xl lg:text-[66px] font-Ovo mb-6">
+            </motion.h3>
+            <motion.h1
+                initial={{ y: -30, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.8, delay: 0.5 }}
+                className="text-center text-gradient-to-r from-green-300 via-yellow-400 to-green-300 text-3xl sm:text-5xl lg:text-[66px] font-Ovo mb-6 dark:text-yellow-400">
                 Frontend Web Developer based in India.
-            </h1>
-            <p className="max-w-2xl mx-auto text-center text-lg md:text-xl font-Ovo mb-6">
+            </motion.h1>
+            <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.7 }}
+                className="max-w-2xl mx-auto text-center text-lg md:text-xl font-Ovo mb-6">
                 I'm a frontend developer from Konaseema, Andhra with 3 years of experience in multiple companies like Lumidad, MediCardia, and freelancing.
-            </p>
+            </motion.p>
             <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
                 <a
                     href="#contact"
